2f76cc221efd955e8da97de2b05f464b5cad9529
Pato Zavala <pazavala@uc.cl>
2021-01-17 17:38:16 -0300

    a cookie was added join with a first template

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app= Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
-    return 'Hello World Flask course by Platzi, your IP is {}'.format(user_ip)
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return render_template('hello.html', user_ip= user_ip)
