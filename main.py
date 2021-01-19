d4dda09947957bc1fb9884e0949441360d4f4424
Pato Zavala <pazavala@uc.cl>
2021-01-18 21:18:22 -0300

    templates were added

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app= Flask(__name__)
 
+todos = ['Todo 1', 'Todo 2', 'Todo 3']
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -12,4 +15,7 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return render_template('hello.html', user_ip= user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos': todos}
+    return render_template('hello.html', **context)
