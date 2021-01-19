764d948a1c8ec99a0b0b32dd96785acfdff4d5c8
Pato Zavala <pazavala@uc.cl>
2021-01-19 19:17:29 -0300

    final version of basic-flask app

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
-
+import unittest
 
 app= Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,6 +16,11 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     passwordField = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -32,13 +37,25 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Authenticated user!')
+
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
